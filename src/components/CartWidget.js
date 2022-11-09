cf72e3df16456ad531abf156a21d1f8c31e141e0
Damián Fiore <d.eze.fiore@gmail.com>
2022-11-09 20:25:48 -0300

    PreEntrega1-FioreDamian

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -2,8 +2,9 @@ import carro from '../carrito.png'
 
 export const CartWitget = () => {
     return(
-        <div>
+        <div className='carritodecompras'>
             <img src={carro} className="carro-logo" alt="carrito" />
+            <h2>0</h2>
         </div>
     )
 }
--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react'
-import {ItemCount} from './ItemCount'
 import { PedirDatos } from '../Helpers/PedirDatos'
 import { Item } from './Item'
 import { ItemDetail } from './ItemDetail'
